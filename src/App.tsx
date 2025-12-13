10d23c6ea8aa4f036db9bac7179a1d6bed2a5294
LENG <hourleng9@gmail.com>
2025-12-13 18:57:18 +0700

    change: change layout detail page

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,7 @@ const App: React.FC = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-        <Layout>
-          <AppRoutes />
-        </Layout>
+        <AppRoutes />
       </BrowserRouter>
     </ThemeProvider>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import HeroSection from "../components/home/HeroSection";
 import ProjectsGrid from "../components/home/ProjectsGrid";
-
+import Layout from "../components/layout/Layout";
 const HomePage: React.FC = () => {
   return (
-    <>
+    <Layout>
       <HeroSection />
       <ProjectsGrid />
-    </>
+    </Layout>
   );
 };
 
